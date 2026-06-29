6126340453e27bdbec52d950b3180ad17fe823d5
Maskhugo <hugoalves.p@outlook.com>
2026-06-29 10:07:35 -0300

    feat:Cor do titulo no cabeçalho alterado

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,7 @@ class _ContadorProdutividadeState extends State<ContadorProdutividade> {
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
-                  color: Color(0xFF333333),
+                  color: Color.fromARGB(255, 255, 0, 0),
                 ),
                 textAlign: TextAlign.center,
               ),
